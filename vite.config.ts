d703039ba8249b055f05ca4d549cd0a19e0482db
Maxime Beauchamp <15185355+baktun14@users.noreply.github.com>
2026-01-13 10:58:47 -0500

    fix: port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3456',
+        target: 'http://localhost:8787',
         changeOrigin: true,
       },
     },
+  },
+  preview: {
+    port: 3000,
   },
 })
